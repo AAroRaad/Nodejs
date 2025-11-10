1fb9d8e9757da331b92005515471a655703ae89b
AAro <aaro.se7en@gmail.com>
2025-11-10 11:28:15 +0330

    Adding Payments and clear bugs

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const PDFDocument = require("pdfkit");
 
@@ -126,6 +127,45 @@ exports.postCartDeleteProduct = async (req, res, next) => {
   try {
     await req.user.removeFromCart(productId);
     res.redirect("/cart");
+  } catch (err) {
+    const error = new Error(err);
+    error.httpStatusCode = 500;
+    return next(error);
+  }
+};
+
+exports.getCheckout = async (req, res, next) => {
+  let products;
+  let total = 0;
+  try {
+    const user = await req.user.populate("cart.items.productId");
+    products = user.cart.items;
+    products.forEach((p) => {
+      total += p.quantity * p.productId.price;
+    });
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: products.map((p) => {
+        return {
+          name: p.productId.title,
+          description: p.productId.description,
+          amount: p.productId * 100,
+          currency: "usd",
+          quantity: p.quantity,
+        };
+      }),
+      success_url: req.protocol + "://" + req.get("host") + "/checkout/success",
+      cancel_url: req.protocol + "://" + req.get("host") + "/checkout/cancelّ",
+    });
+
+    res.render("shop/checkout", {
+      path: "/checkout",
+      pageTitle: "Checkout",
+      products: products,
+      totalSum: total,
+      sessionId: session.id,
+    });
   } catch (err) {
     const error = new Error(err);
     error.httpStatusCode = 500;
